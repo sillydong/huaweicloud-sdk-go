6ffd063d94d82113e5ddb764fae856d8b4518fff
Chen.Zhidong <zhidong.chen@jimeng.io>
2019-03-01 00:17:43 +0800

    update blockstorage

--- openstack/blockstorage/v2/volumes/testing/fixtures.go
+++ openstack/blockstorage/v2/volumes/testing/fixtures.go
@@ -86,6 +86,75 @@ func MockListResponse(t *testing.T) {
 	})
 }
 
+
+func MockDetailResponse(t *testing.T) {
+	th.Mux.HandleFunc("/volumes/detail", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, `
+{
+    "volumes": [
+        {
+            "attachments": [],
+            "availability_zone": "xxx",
+            "bootable": "false",
+            "consistencygroup_id": null,
+            "created_at": "2016-05-25T02:42:10.856332",
+            "description": null,
+            "encrypted": false,
+            "id": "b104b8db-170d-441b-897a-3c8ba9c5a214",
+            "links": [
+                {
+                    "href": "https://volume.localdomain.com:8776/v2/dd14c6ac581f40059e27f5320b60bf2f/volumes/b104b8db-170d-441b-897a-3c8ba9c5a214",
+                    "rel": "self"
+                },
+                {
+                    "href": "https://volume.localdomain.com:8776/dd14c6ac581f40059e27f5320b60bf2f/volumes/b104b8db-170d-441b-897a-3c8ba9c5a214",
+                    "rel": "bookmark"
+                }
+            ],
+            "metadata": {
+                "__openstack_region_name": "pod01.xxx",
+                "a": "b",
+                "quantityGB": "1",
+                "volInfoUrl": "fusionstorage://172.30.64.10/0/FEFEEB07D3924CDEA93C612D4E16882D"
+            },
+            "name": "zjb_u25_test",
+            "os-vol-host-attr:host": "pod01.xxx#SATA",
+            "volume_image_metadata": {},
+            "os-vol-mig-status-attr:migstat": null,
+            "os-vol-mig-status-attr:name_id": null,
+            "os-vol-tenant-attr:tenant_id": "dd14c6ac581f40059e27f5320b60bf2f",
+            "os-volume-replication:driver_data": null,
+            "os-volume-replication:extended_status": null,
+            "replication_status": "disabled",
+            "shareable": false,
+            "multiattach": false,
+            "size": 1,
+            "snapshot_id": null,
+            "source_volid": null,
+            "status": "available",
+            "updated_at": "2016-05-25T02:42:22.341984",
+            "user_id": "b0524e8342084ef5b74f158f78fc3049",
+            "volume_type": "SATA"
+        }
+    ],
+    "volumes_links": [
+        {
+            "href": "https://volume.localdomain.com:8776/v2/dd14c6ac581f40059e27f5320b60bf2f/volumes/detail?limit=1&marker=b104b8db-170d-441b-897a-3c8ba9c5a214",
+            "rel": "next"
+        }
+    ]
+}
+  `)
+	})
+}
+
+
 func MockGetResponse(t *testing.T) {
 	th.Mux.HandleFunc("/volumes/d32019d3-bc6e-4319-9c1d-6722fc136a22", func(w http.ResponseWriter, r *http.Request) {
 		th.TestMethod(t, r, "GET")
@@ -201,4 +270,198 @@ func MockUpdateResponse(t *testing.T) {
 }
         `)
 	})
+}
+
+func MockCreateMetadataResponse(t *testing.T) {
+	th.Mux.HandleFunc("/volumes/8dd7c486-8e9f-49fe-bceb-26aa7e312b66/metadata", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "POST")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+		th.TestHeader(t, r, "Content-Type", "application/json")
+		th.TestHeader(t, r, "Accept", "application/json")
+		th.TestJSONRequest(t, r, `
+{
+    "meta": {
+        "key": "val"
+    }
+}
+      `)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusAccepted)
+
+		fmt.Fprintf(w, `
+{
+    "meta": {
+        "key": "val"
+    }
+}
+    `)
+	})
+}
+
+func MockUpdateMetadataResponse(t *testing.T) {
+	th.Mux.HandleFunc("/volumes/8dd7c486-8e9f-49fe-bceb-26aa7e312b66/metadata", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "PUT")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+		th.TestHeader(t, r, "Content-Type", "application/json")
+		th.TestHeader(t, r, "Accept", "application/json")
+		th.TestJSONRequest(t, r, `
+{
+    "meta": {
+        "key": "val"
+    }
+}
+      `)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusAccepted)
+
+		fmt.Fprintf(w, `
+{
+    "meta": {
+        "key": "val"
+    }
+}
+    `)
+	})
+}
+
+func MockGetMetadataResponse(t *testing.T) {
+	th.Mux.HandleFunc("/volumes/8dd7c486-8e9f-49fe-bceb-26aa7e312b66/metadata", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, `
+{
+    "meta": {
+        "key": "val"
+    }
+}
+    `)
+	})
+}
+
+func MockGetMetadataKeyResponse(t *testing.T) {
+	th.Mux.HandleFunc("/volumes/8dd7c486-8e9f-49fe-bceb-26aa7e312b66/metadata/key", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, `
+{
+    "meta": {
+        "key": "val"
+    }
+}
+    `)
+	})
+}
+
+func MockUpdateMetadataKeyResponse(t *testing.T) {
+	th.Mux.HandleFunc("/volumes/8dd7c486-8e9f-49fe-bceb-26aa7e312b66/metadata/key", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "PUT")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+		th.TestHeader(t, r, "Content-Type", "application/json")
+		th.TestHeader(t, r, "Accept", "application/json")
+		th.TestJSONRequest(t, r, `
+{
+    "meta": {
+        "key": "val"
+    }
+}
+      `)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusAccepted)
+
+		fmt.Fprintf(w, `
+{
+    "meta": {
+        "key": "val"
+    }
+}
+    `)
+	})
+}
+
+func MockDeleteMetadataKeyResponse(t *testing.T) {
+	th.Mux.HandleFunc("/volumes/8dd7c486-8e9f-49fe-bceb-26aa7e312b66/metadata/key", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "DELETE")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+		w.WriteHeader(http.StatusAccepted)
+	})
+}
+
+func MockExtendSizeResponse(t *testing.T) {
+	th.Mux.HandleFunc("/volumes/8dd7c486-8e9f-49fe-bceb-26aa7e312b66/action",
+		func(w http.ResponseWriter, r *http.Request) {
+			th.TestMethod(t, r, "POST")
+			th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+			th.TestHeader(t, r, "Content-Type", "application/json")
+			th.TestHeader(t, r, "Accept", "application/json")
+			th.TestJSONRequest(t, r, `
+{
+    "os-extend":
+    {
+        "new_size": 3
+    }
+}
+          `)
+
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusAccepted)
+
+			fmt.Fprintf(w, `{}`)
+		})
+}
+
+func MockSetBootableResponse(t *testing.T) {
+	th.Mux.HandleFunc("/volumes/8dd7c486-8e9f-49fe-bceb-26aa7e312b66/action",
+		func(w http.ResponseWriter, r *http.Request) {
+			th.TestMethod(t, r, "POST")
+			th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+			th.TestHeader(t, r, "Content-Type", "application/json")
+			th.TestHeader(t, r, "Accept", "application/json")
+			th.TestJSONRequest(t, r, `
+{
+    "os-set_bootable":
+    {
+        "bootable": true
+    }
+}
+          `)
+
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusAccepted)
+
+			fmt.Fprintf(w, `{}`)
+		})
+}
+
+func MockSetReadOnlyResponse(t *testing.T) {
+	th.Mux.HandleFunc("/volumes/8dd7c486-8e9f-49fe-bceb-26aa7e312b66/action",
+		func(w http.ResponseWriter, r *http.Request) {
+			th.TestMethod(t, r, "POST")
+			th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+			th.TestHeader(t, r, "Content-Type", "application/json")
+			th.TestHeader(t, r, "Accept", "application/json")
+			th.TestJSONRequest(t, r, `
+{
+    "os-update_readonly_flag":
+    {
+        "readonly": true
+    }
+}
+          `)
+
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusAccepted)
+
+			fmt.Fprintf(w, `{}`)
+		})
 }
--- openstack/blockstorage/v2/volumes/testing/requests_test.go
+++ openstack/blockstorage/v2/volumes/testing/requests_test.go
@@ -256,3 +256,134 @@ func TestGetWithExtensions(t *testing.T) {
 		t.Errorf("Expected error when providing non-pointer struct")
 	}
 }
+
+
+func TestGetMetadata(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	MockGetMetadataResponse(t)
+
+	v, err := volumes.GetMetadata(client.ServiceClient(), "8dd7c486-8e9f-49fe-bceb-26aa7e312b66").ExtractMetadata()
+	th.AssertNoErr(t, err)
+
+	th.AssertEquals(t, v["key"], "val")
+}
+
+func TestCreateMetadata(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	MockCreateMetadataResponse(t)
+
+	option := volumes.MetadataOpts{
+		Metadata: map[string]string{
+			"key": "val",
+		},
+	}
+
+	v, err := volumes.CreateMetadata(client.ServiceClient(), "8dd7c486-8e9f-49fe-bceb-26aa7e312b66", &option).ExtractMetadata()
+	th.AssertNoErr(t, err)
+
+	th.AssertEquals(t, v["key"], "val")
+}
+
+func TestUpdateMetadata(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	MockUpdateMetadataResponse(t)
+
+	option := volumes.MetadataOpts{
+		Metadata: map[string]string{
+			"key": "val",
+		},
+	}
+
+	v, err := volumes.UpdateMetadata(client.ServiceClient(), "8dd7c486-8e9f-49fe-bceb-26aa7e312b66", &option).ExtractMetadata()
+	th.AssertNoErr(t, err)
+
+	th.AssertEquals(t, v["key"], "val")
+}
+
+func TestGetMetadataKey(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	MockGetMetadataKeyResponse(t)
+
+	v, err := volumes.GetMetadataKey(client.ServiceClient(), "8dd7c486-8e9f-49fe-bceb-26aa7e312b66", "key").ExtractMetadata()
+	th.AssertNoErr(t, err)
+
+	th.AssertEquals(t, v["key"], "val")
+}
+
+func TestUpdateMetadataKey(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	MockUpdateMetadataKeyResponse(t)
+
+	option := volumes.MetadataOpts{
+		Metadata: map[string]string{
+			"key": "val",
+		},
+	}
+
+	v, err := volumes.UpdateMetadataKey(client.ServiceClient(), "8dd7c486-8e9f-49fe-bceb-26aa7e312b66", "key", &option).ExtractMetadata()
+	th.AssertNoErr(t, err)
+
+	th.AssertEquals(t, v["key"], "val")
+}
+
+func TestDeleteMetadataKey(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	MockDeleteMetadataKeyResponse(t)
+
+	res := volumes.DeleteMetadataKey(client.ServiceClient(), "8dd7c486-8e9f-49fe-bceb-26aa7e312b66", "key")
+	th.AssertNoErr(t, res.Err)
+}
+
+func TestExtendSize(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	MockExtendSizeResponse(t)
+
+	options := &volumes.ExtendSizeOpts{
+		NewSize: 3,
+	}
+
+	err := volumes.ExtendSize(client.ServiceClient(), "8dd7c486-8e9f-49fe-bceb-26aa7e312b66", options).ExtractErr()
+	th.AssertNoErr(t, err)
+}
+
+func TestSetBootable(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	MockSetBootableResponse(t)
+
+	options := &volumes.SetBootableOpts{
+		Bootable: true,
+	}
+
+	err := volumes.SetBootable(client.ServiceClient(), "8dd7c486-8e9f-49fe-bceb-26aa7e312b66", options).ExtractErr()
+	th.AssertNoErr(t, err)
+}
+
+func TestSetReadOnly(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	MockSetReadOnlyResponse(t)
+
+	options := &volumes.SetReadOnlyOpts{
+		ReadOnly: true,
+	}
+
+	err := volumes.SetReadOnly(client.ServiceClient(), "8dd7c486-8e9f-49fe-bceb-26aa7e312b66", options).ExtractErr()
+	th.AssertNoErr(t, err)
+}
